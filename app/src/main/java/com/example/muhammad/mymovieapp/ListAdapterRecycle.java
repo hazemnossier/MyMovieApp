25f67b7271c7fcc5a56d001315720eef42cdcd22
Zo_Om <thetechghost@gmail.com>
2016-11-20 21:20:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/muhammad/mymovieapp/ListAdapterRecycle.java
@@ -0,0 +1,31 @@
+package com.example.muhammad.mymovieapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Muhammad on 20/11/2016.
+ */
+
+public class ListAdapterRecycle extends RecyclerView.Adapter<ListAdapterRecycle.ViewHolder>{
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView flickerImageView;
+        RelativeLayout flickerImageContainer;
+        ProgressBar progressBar;
+        ImageButton favBtn;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            flickerImageView = (ImageView) itemView.findViewById(R.id.flickerImageView);
+            flickerImageContainer = (RelativeLayout) itemView.findViewById(R.id.ImageContainer);
+            progressBar = (ProgressBar) itemView.findViewById(R.id.progressBar);
+            favBtn = (ImageButton) itemView.findViewById(R.id.favIcon);
+        }
+}
